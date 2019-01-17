b054f8686c7ce7228f2e447875d38a79267febc8
Boyden <d_boyden@tss-hr420-dm05.fc.ca>
2019-01-17 17:30:51 -0500

    done for this clas

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
 (() => {
 
 // create a component first
-const UserComponent = {
-  props: ['name', 'role'],
-
-
-  template: "#userstemplate",
-
-  created: function(){
-    console.log("im aliiiiiiiveee!");
-  },
-
-  methods: {
-    logFromChild() {
-      console.log("logged from component");
-    },
-
-    passEvent() {
-      this.$emit('shoutup');
-    }
-  }
+const HomePageComponent = {
+  template: "<h2> This is the home page</h2>"
 }
+
+const UsersPageComponent = {
+  template: "<h2> This is the users page</h2>"
+}
+
+const contactPageComponent = {
+  template: " <h2> This is the contact page </h2>"
+}
+
+const routes = [
+  {path: '/', name: 'home', component: HomePageComponent},
+  {path: '/contact', name: 'contact' , component: contactPageComponent},
+  {path: '/users', name: 'users' , component: UsersPageComponent}
+]
+
+
+const router = new VueRouter({
+  routes
+});
+
 // then your vue instance
 
 const vm = new Vue({
@@ -42,9 +45,12 @@ created: function() {
 },
 
   components: {
-    'activeusers' : UserComponent
-  }
+    'homepagecomponent': HomePageComponent,
+    'userspagecomponent': UsersPageComponent,
+    'contactcomponent': contactPageComponent
 
-})
+  },
+  router
+});
 
 })();
